3015300341fedc6a7c83a4d8f54129c8bc3a1796
pc <pc@192.168.1.6>
2024-05-10 15:57:56 -0500

    carga inicial

---
+++ src/main/java/com/mycompany/prueba/Prueba.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author pc
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hola salón!");
+    }
+}
